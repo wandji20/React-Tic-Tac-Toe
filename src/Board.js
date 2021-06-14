5fe9c305c9552c6a50ce1e74fe6a6e6800121033
wandji20 <wbertrand10@gmail.com>
2021-06-14 11:54:38 +0100

    Uplift handleClick method from Square to Board component

--- src/Board.js
+++ src/Board.js
@@ -1,12 +1,29 @@
-import { div } from 'prelude-ls'
 import React, { Component } from 'react'
 import Square from './Square'
 
 export default class Board extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      squares : Array(9).fill(null)
+    }
+    this.handleClick = this.handleClick.bind(this)
+  }
+
+  handleClick(i){
+    let newSquares = this.state.squares.slice()
+    newSquares[i] = 'X'
+    this.setState({
+      squares: newSquares
+    })
+    // console.log(`${i}`)
+  }
+
   renderSquare(i){
-    return <Square value = {i}/>
+    return <Square value = {this.state.squares[i]} handleClick = {()=>{this.handleClick(i)}} />
   }
   render() {
+    // console.log(this.state.squares)
     const status = 'Next Player: X'
     return (
       <div>
--- src/Square.js
+++ src/Square.js
@@ -1,22 +1,22 @@
 import React, { Component } from 'react'
 
 export default class Square extends Component {
-  constructor(props){
-    super(props)
-    this.state = {
-      value: null
-    }
-    this.someFunc = this.someFunc.bind(this)
-  }
-  someFunc(){
-    this.setState({
-      value: 'X'
-    })
-  }
+  // constructor(props){
+  //   super(props)
+  //   this.state = {
+  //     value: null
+  //   }
+  //   this.handleClick = this.handleClick.bind(this)
+  // }
+  // handleClick(){
+  //   this.setState({
+  //     value: 'X'
+  //   })
+  // }
   render() {
     return (
-      <button className="square" onClick = {this.someFunc}>
-        {this.state.value}
+      <button className="square" onClick = {this.props.handleClick}>
+        {this.props.value}
       </button>
     )
   }
